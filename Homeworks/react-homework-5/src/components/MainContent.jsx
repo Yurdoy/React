3530b19fac8ad8c135a1057e4923f0d36a9e6394
Yurdoy <yurdoy@gmail.com>
2025-05-10 07:34:51 +0100

    React homework 5 completed

--- Homeworks/react-homework-5/src/components/MainContent.jsx
+++ Homeworks/react-homework-5/src/components/MainContent.jsx
@@ -3,7 +3,9 @@ import "../styles/MainContent.css";
 import appleLogo from "../assets/images/apple-icon.svg";
 import appleLogoBlue from "../assets/images/apple-blue-icon.svg";
 import googleLogo from "../assets/images/google-icon.svg";
+import googleLogoBlue from "../assets/images/google-blue-icon.svg";
 import twitterLogo from "../assets/images/twitter-icon.svg";
+import twitterLogoBlue from "../assets/images/twitter-blue-icon.svg";
 
 const MainContent = () => {
   return (
@@ -24,8 +26,30 @@ const MainContent = () => {
             className="apple-icon hover"
           />
         </button>
-        <button className="google-btn" type="button"></button>
-        <button className="twitter-btn" type="button"></button>
+        <button className="google-btn" type="button">
+          <img
+            src={googleLogo}
+            alt="Google Icon Green"
+            className="google-icon default"
+          />
+          <img
+            src={googleLogoBlue}
+            alt="Google Icon Blue"
+            className="google-icon hover"
+          />
+        </button>
+        <button className="twitter-btn" type="button">
+          <img
+            src={twitterLogo}
+            alt="Twitter Icon Green"
+            className="twitter-icon default"
+          />
+          <img
+            src={twitterLogoBlue}
+            alt="Twitter Icon Blue"
+            className="twitter-icon hover"
+          />
+        </button>
       </div>
     </main>
   );
