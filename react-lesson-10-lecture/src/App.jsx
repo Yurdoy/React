86daec39e44afa4d365f735d38dc389aa8fd75eb
Yurdoy <yurdoy@gmail.com>
2025-06-07 21:40:17 +0200

    addPost, updatePost, deletePost, editPost functions added.

--- react-lesson-10-lecture/src/App.jsx
+++ react-lesson-10-lecture/src/App.jsx
@@ -3,10 +3,10 @@ import Header from "./components/Header";
 import PostForm from "./components/PostForm";
 import PostList from "./components/PostList";
 import styles from "./App.module.css";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
-  const [post, setPost] = useState([]);
+  const [posts, setPosts] = useState([]);
   const [editingPost, setEditingPost] = useState(null);
 
   useEffect(() => {
@@ -17,7 +17,7 @@ function App() {
         );
         if (!res.ok) throw new Error("Failed to fetch posts");
         const data = await res.json();
-        setPost(data);
+        setPosts(data);
       } catch (err) {
         console.log(err);
       }
@@ -25,13 +25,37 @@ function App() {
     fetchPosts();
   }, []);
 
+  const addPost = (newPost) => {
+    setPosts((prev) => [newPost, ...prev]);
+  };
+
+  const updatePost = (updatePost) => {
+    setPosts((prev) =>
+      prev.map((post) => (post.id === updatePost.id ? updatePost : post))
+    );
+    setEditingPost(null);
+  };
+
+  const deletePost = (id) => {
+    setPosts((prev) => prev.filter((post) => post.id !== id));
+  };
+
+  const editPost = (post) => {
+    setEditingPost(post);
+  };
+
   return (
     <>
       <div>
         <Header />
         <main className={styles.main}>
-          <PostList />
-          <PostForm />
+          <PostList posts={posts} onEdit={editPost} onDelete={deletePost} />
+          <PostForm
+            onAdd={addPost}
+            onUpdate={updatePost}
+            editingPost={editingPost}
+            onCancel={() => setEditingPost(null)}
+          />
         </main>
       </div>
     </>
