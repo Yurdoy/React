187b5723adfd6c2954be34caa18564e658563806
Yurdoy <yurdoy@gmail.com>
2025-06-07 16:24:00 +0200

    useEffect in App.jsx created to fetch posts from API once on mount.

--- react-lesson-10-lecture/src/App.jsx
+++ react-lesson-10-lecture/src/App.jsx
@@ -3,8 +3,28 @@ import Header from "./components/Header";
 import PostForm from "./components/PostForm";
 import PostList from "./components/PostList";
 import styles from "./App.module.css";
+import { useEffect } from "react";
 
 function App() {
+  const [post, setPost] = useState([]);
+  const [editingPost, setEditingPost] = useState(null);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const res = await fetch(
+          "https://683ef3211cd60dca33ddb0f3.mockapi.io/posts"
+        );
+        if (!res.ok) throw new Error("Failed to fetch posts");
+        const data = await res.json();
+        setPost(data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchPosts();
+  }, []);
+
   return (
     <>
       <div>
