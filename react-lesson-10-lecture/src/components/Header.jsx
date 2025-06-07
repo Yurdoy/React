0d530a1088fc240e5feb32a2cde14d8b224a13bc
Yurdoy <yurdoy@gmail.com>
2025-06-07 14:29:57 +0200

    Header.module.css created. Styles for Header component created.

--- react-lesson-10-lecture/src/components/Header.jsx
+++ react-lesson-10-lecture/src/components/Header.jsx
@@ -1,7 +1,27 @@
 import React from "react";
+import styles from "../styles/Header.module.css";
+import logo from "../assets/logo.svg";
 
 const Header = () => {
-  return <div></div>;
+  return (
+    <header className={styles.header}>
+      <img className={styles.logo} src={logo} alt="Logo" />
+      <nav className={styles.nav}>
+        <a className={styles.main} href="/link">
+          Main
+        </a>
+        <a className={styles.music} href="/link">
+          Music
+        </a>
+        <a className={styles.community} href="/link">
+          Community
+        </a>
+        <a className={styles.friends} href="/link">
+          Friends
+        </a>
+      </nav>
+    </header>
+  );
 };
 
 export default Header;
