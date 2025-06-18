6b03a89b20bea84b802ff80152ab1fe1ef1f0d53
Yurdoy <yurdoy@gmail.com>
2025-06-18 10:41:52 +0200

    Post.module.css - userLogo styled

--- react-lesson-10-lecture/src/components/Post.jsx
+++ react-lesson-10-lecture/src/components/Post.jsx
@@ -1,15 +1,23 @@
 import React from "react";
+import styles from "../styles/Post.module.css";
+import userLogo from "../assets/user-logo.svg";
 
 const Post = ({ title, body, onDelete }) => {
   return (
-    <article>
-      <header>
-        <h2>{title}</h2>
-      </header>
-      <p>{body}</p>
-      <button onClick={onDelete} title="Delete Post">
-        Delete
-      </button>
+    <article className={styles.article}>
+      <div className={styles.posts}>
+        <div className={styles.userLogo}>
+          <img src={userLogo} alt="User Logo" />
+          <p>User logo</p>
+        </div>
+        <header>
+          <h2>{title}</h2>
+        </header>
+        <p>{body}</p>
+        <button onClick={onDelete} title="Delete Post">
+          Delete
+        </button>
+      </div>
     </article>
   );
 };
